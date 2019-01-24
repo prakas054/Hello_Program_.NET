2a5b42d0ced1c7f362276198ad695cfe304f8f9c
Prakash kumar <prakas054@gmail.com>
2019-01-24 09:27:23 +0530

    Dotnet First commit

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World" + " \n");
+            Console.WriteLine("Please enter your name" + " \n");
+            string Name = Console.ReadLine();
+            Console.WriteLine( " \n"+ "Hello {0}, Your are most welcome", Name );
+            Console.ReadKey();
+        }
+    }
+}
